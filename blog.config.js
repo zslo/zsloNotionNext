1361491213665fd363c5bc655e25d22fa8256df6
zslo <53238398+zslo@users.noreply.github.com>
2026-07-12 16:38:57 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,16 +17,16 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '香蕉鱼', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '城市中的游牧者 出租屋里的穴居人', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://zslo.top', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
   BEI_AN_GONGAN: process.env.NEXT_PUBLIC_BEI_AN_GONGAN || '', // 公安备案号，例如 '浙公网安备3xxxxxxxx8号'
 
   // RSS订阅
-  ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || true, // 是否开启RSS订阅功能
+  ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || false, // 是否开启RSS订阅功能
 
   // 其它复杂配置
   // 原配置文件过长，且并非所有人都会用到，故此将配置拆分到/conf/目录下, 按需找到对应文件并修改即可
@@ -70,7 +70,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    '阴影于黑夜里逃逸,意识于洞穴内排泄',
+    '阴影于黑夜里逃逸 意识于洞穴内排泄',
 
   // 欢迎语打字效果类型速度
   GREETING_WORDS_TYPE_SPEED:
@@ -78,7 +78,7 @@ const BLOG = {
 
   // 欢迎语打字效果回退速度
   GREETING_WORDS_BACK_SPEED:
-    process.env.NEXT_PUBLIC_GREETING_WORDS_BACK_SPEED || 100,
+    process.env.NEXT_PUBLIC_GREETING_WORDS_BACK_SPEED || 300,
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
