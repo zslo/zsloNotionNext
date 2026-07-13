6ab9733d2065bc0b8e9916a494d3af19fdd8b40a
zslo <53238398+zslo@users.noreply.github.com>
2026-07-13 15:35:11 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '香蕉鱼', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '城市中的游牧者\n出租屋里的穴居人', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'www.zslo.top', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.zslo.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
